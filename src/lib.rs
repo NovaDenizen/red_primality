5ac221513f06180f572454203ecd5b7bd0f3573d
Ken Bateman <novadenizen@gmail.com>
2019-08-29 17:31:22 -0400

    I think it works.

--- src/lib.rs
+++ src/lib.rs
@@ -15,27 +15,13 @@
 /// of tests to efficiently and determinstically determine primality for all integers inn the `u64`
 /// range.
 ///
+/// https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
+///
 pub fn is_u64_prime(n: u64) -> bool
 {
-        // if n < 9,080,191, it is enough to test a = 31 and 73;
-        // if n < 25,326,001, it is enough to test a = 2, 3, and 5;
-        // if n < 3,215,031,751, it is enough to test a = 2, 3, 5, and 7;
-        // if n < 4,759,123,141, it is enough to test a = 2, 7, and 61;
-        // if n < 1,122,004,669,633, it is enough to test a = 2, 13, 23, and 1662803;
-        // if n < 2,152,302,898,747, it is enough to test a = 2, 3, 5, 7, and 11;
-        // if n < 3,474,749,660,383, it is enough to test a = 2, 3, 5, 7, 11, and 13;
-        // if n < 341,550,071,728,321, it is enough to test a = 2, 3, 5, 7, 11, 13, and 17.
-        // Using the work of Feitsma and Galway enumerating all base 2 pseudoprimes in 2010, this was
-        // extended (see OEIS: A014233), with the first result later shown using different methods in
-        // Jiang and Deng:[10]
-        //
-        // if n < 3,825,123,056,546,413,051, it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, and
-        // 23.
-        // if n < 18,446,744,073,709,551,616 = 264, it is enough to test a = 2, 3, 5, 7, 11, 13, 17,
-        // 19, 23, 29, 31, and 37.
     if n == 2 || n == 3 {
         true
-    } else if n & 1 == 0 {
+    } else if n & 1 == 0 || n < 5 {
         false
     } else if n < 2_047 {
         // if n < 2,047, it is enough to test a = 2;
@@ -63,18 +49,125 @@ pub fn is_u64_prime(n: u64) -> bool
     } 
 }
 
-fn sprp_u64(n: u64, p: u8) -> bool {
-    unimplemented!();
+
+fn sprp_u64(n: u64, a: u8) -> bool {
+    let a = a as u64;
+    let d = n - 1;
+    let r = d.trailing_zeros();
+    let d = d >> r;
+    assert_eq!((1 << r) * d + 1, n);
+    let mut x = pow_mod_u64(a, d, n);
+    if x == 1 || x + 1 == n {
+        return true;
+    }
+    for _ in 1..r {
+        x = (x*x) % n;
+        if x + 1 == n {
+            return true;
+        }
+    }
+    false
 }
 
-fn sprp_u128(n: u128, p: u8) -> bool {
-    unimplemented!();
+// assumes both x*x and m*m < std::u64::MAX
+fn pow_mod_u64(mut x: u64, mut p: u64, m: u64) -> u64 {
+    let mut res = 1;
+    loop {
+        // loop invariant: res * x^p congruent to original x^p
+        if p & 1 == 1 {
+            res = (res * x) % m;
+            p -= 1;
+        }
+        if p > 0 {
+            x = (x * x) % m;
+            p /= 2;
+        } else {
+            break;
+        }
+    }
+    res
+}
+// assumes both x*x and m*m < std::u128::MAX
+fn pow_mod_u128(mut x: u128, mut p: u128, m: u128) -> u128 {
+    let mut res = 1;
+    loop {
+        // loop invariant: res * x^p congruent to original x^p
+        if p & 1 == 1 {
+            res = (res * x) % m;
+            p -= 1;
+        }
+        if p > 0 {
+            x = (x * x) % m;
+            p /= 2;
+        } else {
+            break;
+        }
+    }
+    res
+}
+
+fn sprp_u128(n: u128, a: u8) -> bool {
+    let a = a as u128;
+    let d = n - 1;
+    let r = d.trailing_zeros();
+    let d = d >> r;
+    assert_eq!((1 << r) * d + 1, n);
+    let mut x = pow_mod_u128(a, d, n);
+    if x == 1 || x + 1 == n {
+        return true;
+    }
+    for _ in 1..r {
+        x = (x*x) % n;
+        if x + 1 == n {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+    use primal::Sieve;
+
+    fn test_prime_consistency(sieve: &Sieve, n: u64) {
+        assert_eq!(is_u64_prime(n), sieve.is_prime(n as usize), "Primality test inconsistent for n={}", n);
     }
+    const LIMIT: u64 = 1_000_000;
+    #[test]
+    fn small_numbers() {
+        let sieve = Sieve::new(LIMIT as usize);
+        for i in 0..LIMIT {
+            test_prime_consistency(&sieve, i);
+        }
+    }
+    fn excessive_sprp_test(n: u64) -> bool {
+        assert!(n > LIMIT);
+        let n = n as u128;
+        for i in 0..100 {
+            let k = 3 + i*2;
+            if !sprp_u128(n, k as u8) {
+                return false;
+            }
+        }
+        true
+    }
+    fn test_prime_excessive(n: u64) {
+        if n < LIMIT {
+            return;  // donn't bother testinng small ones.
+        }
+        assert_eq!(excessive_sprp_test(n), is_u64_prime(n), "excessive test failed f= rn={}", n);
+    }
+    #[test]
+    fn big_numbers() {
+        use std::num::Wrapping;
+        let inc = Wrapping(1_234_567_123_456_892);  // an arbitrarily chosen big even number
+        let mut x = Wrapping(1);
+        let count = 10_000;
+        for _ in 0..count {
+            x += inc;
+            test_prime_excessive(x.0);
+        }
+    }
+
 }
