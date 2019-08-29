c2ffb1641497321078064adf003a300281b4353d
Ken Bateman <novadenizen@gmail.com>
2019-08-29 17:50:24 -0400

    fixed documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 
-#![deny(missing_docs)]
 
 //! red_primality provides a quick accurate primality test for all u64.
+//!
+//! # Example
+//!
+//! ```
+//! use red_primality::is_u64_prime;
+//!
+//! fn main() {
+//!     assert!(is_u64_prime(5));
+//!     assert!(!is_u64_prime(6));
+//! }
+
+#![deny(missing_docs)]
 
 /// Determines if the given parameter is prime.
 ///
